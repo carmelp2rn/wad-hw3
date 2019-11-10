f33407194a73098a4571d3fce483d777dcf57939
Toomas Lange <toomaslange@gmail.com>
2019-11-10 09:16:40 +0200

    Task1
    Task1 Classes

---
+++ src/models/User.js
@@ -0,0 +1,11 @@
+export default class User {
+
+    constructor(firstName, lastName, birthdate, faculty, gpa){
+        this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthdate = birthdate;
+		this.faculty = faculty;
+		this.gpa = gpa;
+      
+    }
+}
