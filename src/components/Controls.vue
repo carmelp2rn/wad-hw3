beecc43e9f84dd6ea99e5ad61f1f543297e1f807
kristinalillo <kristina.lillo@gmail.com>
2019-11-10 18:13:09 +0200

    Controls, Courses, Header and Profile classes added. Started task 2

---
+++ src/components/Controls.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="controls">
+        <button id="profile-button" class="pill" :class="{active: profileIsActive}" @click="toggleActive(profileIsActive)">Profile</button>
+        <button id="courses-button" class="pill" :class="{active: coursesIsActive}" @click="toggleActive(coursesIsActive)">Courses</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Controls",
+        data: () => {
+            return{
+                profileIsActive: true,
+                coursesIsActive: false,
+            }
+        },
+        methods: {
+            toggleActive: function (isActive) {
+                if(!isActive){
+                    this.profileIsActive = !this.profileIsActive;
+                    this.coursesIsActive = !this.coursesIsActive;
+                    this.$emit('toggle')
+                }
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .controls .pill {
+        border: 1px solid #cbcbcb;
+        background-color: #eaeaea;
+        padding: 10px;
+        border-bottom-left-radius: 4px;
+        border-bottom-right-radius: 4px;
+        border-top: none;
+        margin-top: -1px;
+        outline: none !important;
+    }
+
+    .controls .pill.active {
+        background-color: #ffffff;
+        border-top: 1px solid #ffffff;
+    }
+
+    .controls .pill:hover {
+        cursor: pointer;
+    }
+
+</style>
---
+++ src/components/Courses.vue
@@ -0,0 +1,134 @@
+<template>
+    <div id="courses-container" class="tab" :class="{active: isActive}">
+        <h1 class="title">Courses</h1>
+        <table id="courses">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Course Title</th>
+                <th>Semester</th>
+                <th>Grade</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(course, index) in courses" :key="index">
+                <td>{{index+1}}</td>
+                <td>{{course.title}}</td>
+                <td>{{course.semester}}</td>
+                <td>{{course.grade}}</td>
+            </tr>
+            </tbody>
+        </table>
+        <br>
+        <br>
+        <div>
+            <button @click='toggleFields' id="add-course-button" class="blue-button">+</button>
+            <span v-show='toggle'>
+                <input class="input" type="text" v-model="title" id="title" placeholder="Course title">
+                <input class="input" type="number" v-model="semester" min="1" max="8" placeholder="Semester" id="semester">
+                <input class="input" type="number" v-model="grade" min="0" max="100" placeholder="Grade" id="grade">
+                <button @click="addButton" class="green-button" id="save-course">Save</button>
+                <button @click='cancelButton' class="grey-button" id="cancel-course">Cancel</button>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Course from "../models/Course";
+    export default {
+        el: '#add-course',
+        name: "Courses",
+        data: () => {
+            return {
+                toggle:false,
+                isActive: false,
+                title: "",
+                semester: "",
+                grade: "",
+                courses: [
+                    new Course("Agile Software development",1,82),
+                    new Course("System modeling",1,85),
+                    new Course("Object-oriented programming",2,99),
+                    new Course("Estonian language Level A2",2,65)
+                ]
+            }
+        },
+        methods: {
+            toggleCourses: function () {
+                this.isActive = !this.isActive;
+            },
+            resetFields: function(){
+                this.title = "";
+                this.semester = "";
+                this.grade = "";
+            },
+            toggleFields: function(){
+                this.toggle = !this.toggle;
+            },
+            addButton: function () {
+                let title = this.title;
+                let semester = this.semester;
+                let grade = this.grade;
+                this.courses.push(new Course(title, semester, grade));
+                this.toggleFields();
+                this.resetFields();
+            },
+            cancelButton: function(){
+                this.toggleFields();
+                this.resetFields();
+            },
+
+            }
+    }
+</script>
+
+<style scoped>
+    .tab {
+        display: none;
+    }
+    .tab.active {
+        display: block;
+    }
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    table th {
+        padding: 8px 12px;
+        text-align: left;
+        border: 1px solid #cbcbcb;
+        background-color: #03A9F4;
+        color: #ffffff;
+    }
+    table td {
+        padding: 8px 12px;
+        border: 1px solid #cbcbcb;
+    }
+    .blue-button {
+        background-color: #2196F3;
+        color: #ffffff;
+        border: none;
+        padding: 10px 20px;
+    }
+    .green-button {
+        background-color: #69f378;
+        color: #ffffff;
+        border: none;
+        padding: 10px 10px;
+    }
+    .grey-button {
+        background-color: #e1e8e6;
+        color: #ffffff;
+        border: none;
+        padding: 10px 20px;
+    }
+    .input {
+        border: 1px solid #cccccc;
+        padding: 10px 20px;
+        min-width: 135px;
+    }
+    #add-course {
+        display: none;
+    }
+</style>
